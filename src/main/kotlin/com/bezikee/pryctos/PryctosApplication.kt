3a6b519ee52532bc16a8711c60d61870ab00a039
Andres Rubio <andresfra92@gmail.com>
2021-11-17 00:02:43 -0400

    Initial commit

---
+++ src/main/kotlin/com/bezikee/pryctos/PryctosApplication.kt
@@ -0,0 +1,11 @@
+package com.bezikee.pryctos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PryctosApplication
+
+fun main(args: Array<String>) {
+    runApplication<PryctosApplication>(*args)
+}
---
+++ src/test/kotlin/com/bezikee/pryctos/PryctosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bezikee.pryctos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PryctosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
